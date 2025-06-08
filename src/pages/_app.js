cd30dc4318b9b8b4fa141c57cf75941c80182a34
Anmol023 <anmolgupta023@gmail.com>
2025-06-08 15:26:54 +0530

    added meta

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <title>Anmol Gupta</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"></meta>
       </Head>
       <Component {...pageProps} />
       <Analytics/>
